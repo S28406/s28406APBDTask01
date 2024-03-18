83e7dc2aebb7f8ad0413750071e185550062be50
S28406 <s28406@pjwstk.edu.pl>
2024-03-18 10:13:48 +0100

    added feature to find max

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@
 Console.WriteLine("Modification 3");
 int[] arr = { 1, 2, 3, 4, 5, 6, 7, 7, 9};
 Console.WriteLine(CalculateAverage(arr));
+Console.WriteLine(FindMax(arr));
 
 static double CalculateAverage(int[] numbers)
 {
@@ -12,4 +13,17 @@ static double CalculateAverage(int[] numbers)
         sum += num;
     }
     return (double)sum / numbers.Length;
+}
+
+static int FindMax(int[] numbers)
+{
+    int max = -1;
+    foreach (int num in numbers)
+    {
+        if (num > max)
+        {
+            max = num;
+        }
+    }
+    return max;
 }
